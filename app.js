b5ecd75e68eef8797ec7763e7feaf6b5a94c53b5
coque <coquelet.samuel@gmail.com>
2021-12-21 11:12:36 +0100

    Version jQuery

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-let humide = document.getElementById('humide');
-let vent = document.getElementById('vent');
-let temps = document.getElementById('temp');
-let tempMin = document.getElementById('tempMin');
-let tempMax = document.getElementById('tempMax');
-let but = document.getElementById('but');
-let ville = document.getElementById('ville');
+$('#but').on('click', function () {
 
-but.addEventListener('click', function () {
-
-    const urlM = 'https://api.openweathermap.org/data/2.5/weather?q='+ ville.value +'&appid=54a4cae3c77e0a37f7e28d0687911172&units=metric&lang=fr'
+    const urlM = 'https://api.openweathermap.org/data/2.5/weather?q='+ $('#ville').val() +'&appid=54a4cae3c77e0a37f7e28d0687911172&units=metric&lang=fr'
     const xhr = new XMLHttpRequest();
     xhr.open('GET',urlM);
     xhr.responseType = 'json';
@@ -18,15 +10,13 @@ but.addEventListener('click', function () {
         if(xhr.status !== 200) {
             return;
         }
-
         let reponse = xhr.response;
-        temps.innerHTML = Math.ceil(reponse.main.temp) + ' '+ ' °C';
-        vent.innerHTML = Math.ceil(reponse.wind.speed) + ' '+ ' km/H';
-        humide.innerHTML = Math.ceil(reponse.main.humidity) + ' '+ ' %';
-        tempMin.innerHTML = Math.ceil(reponse.main.temp_min) + ' '+ ' °C';
-        tempMax.innerHTML = Math.ceil(reponse.main.temp_max) + ' '+ ' °C';
+        $('#temp').html(Math.ceil(reponse.main.temp) + ' '+ ' °C');
+        $('#vent').html(Math.ceil(reponse.wind.speed) + ' '+ ' km/H');
+        $('#humide').html(Math.ceil(reponse.main.humidity) + ' '+ ' %');
+        $('#tempMin').html(Math.ceil(reponse.main.temp_min) + ' '+ ' °C');
+        $('#tempMax').html(Math.ceil(reponse.main.temp_max) + ' '+ ' °C');
     }
-
     xhr.send();
 })
 
